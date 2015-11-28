843c5f5d4af43f452755f22d603233a832ad2239
Philipp <philipp.kropik@gmail.com>
2015-11-28 07:36:49 +0100

    Update

--- src/Bruch.java
+++ src/Bruch.java
@@ -8,7 +8,7 @@ public class Bruch {
 	public Bruch (int zaehler, int nenner){
 		this.zaehler =zaehler;
 		this.nenner = nenner;
-	}
+		}
 
 
 	public int getZaehler() {
@@ -31,20 +31,20 @@ public class Bruch {
 	public Bruch multiplicate(Bruch b2){
 		int nzaehler;
 		int nnenner;
-		nzaehler= b2.zaehler * b2.zaehler;
-		nnenner= b2.nenner * b2.nenner;
-		Bruch bruchb2 = new Bruch(nzaehler, nnenner);
-		System.out.println(bruchb2.zaehler+"/"+bruchb2.nenner);
-		return bruchb2;
+		nzaehler= this.zaehler * b2.zaehler;
+		nnenner= this.nenner * b2.nenner;
+		Bruch ergebniss = new Bruch(nzaehler, nnenner);
+		//System.out.println(bruchb2.zaehler+"/"+bruchb2.nenner);
+		return ergebniss;
 	}
 	public Bruch multiplicate2(Bruch b2,Bruch b3){
 		int nzaehler;
 		int nnenner;
-		nzaehler= b2.zaehler * b2.zaehler * b3.zaehler;
-		nnenner= b2.nenner * b2.nenner * b3.nenner;
-		Bruch bruchb3 = new Bruch(nzaehler, nnenner);
-		System.out.println(bruchb3.zaehler+"/"+bruchb3.nenner);
-		return bruchb3;
+		nzaehler= this.zaehler * b2.zaehler * b3.zaehler;
+		nnenner= this.nenner * b2.nenner * b3.nenner;
+		Bruch ergebniss = new Bruch(nzaehler, nnenner);
+		//System.out.println(bruchb3.zaehler+"/"+bruchb3.nenner);
+		return ergebniss;
 	}
 
 }
--- src/Uebung1.java
+++ src/Uebung1.java
@@ -4,17 +4,19 @@ public class Uebung1 {
 	public static void main(String[] args) 
 	{
 		double ergebniss;
-		Bruch Test = new Bruch(5,3);
+		Bruch Test = new Bruch(7,3);
 		Bruch Test2 = new Bruch(3,5);
+		Bruch Test3 = new Bruch(1,2);
 		
 		Test.print();
-		ergebniss = Test.toDecimal();
-		System.out.println(ergebniss);
-
-		Test2.multiplicate(Test);
-		Test.multiplicate2(Test, Test2);
-
 		
+		System.out.println(Test.toDecimal());
+
+		Bruch erg = Test2.multiplicate(Test);
+		erg.print();
+		
+		erg= Test3.multiplicate2(Test, Test2);
+		erg.print();
 		
 	}
 
