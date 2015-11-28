e7e3e60b2d0ff6944914af063d7dd7fef2b58513
Philipp <philipp.kropik@gmail.com>
2015-11-28 10:08:08 +0100

    --

--- src/Bruch.java
+++ src/Bruch.java
@@ -3,11 +3,14 @@ public class Bruch {
 	
 	private int zaehler;
 	private int nenner;
+	public static int Counter = 0;
+	public static int berechnungen =0;
 
 	
 	public Bruch (int zaehler, int nenner){
 		this.zaehler =zaehler;
 		this.nenner = nenner;
+		Counter++;
 		}
 
 
@@ -21,6 +24,7 @@ public class Bruch {
 	
 	public double toDecimal(){
 		double decimal = zaehler*1.0/nenner;
+		berechnungen++;
 		return decimal;
 	}
 	
@@ -34,17 +38,36 @@ public class Bruch {
 		nzaehler= this.zaehler * b2.zaehler;
 		nnenner= this.nenner * b2.nenner;
 		Bruch ergebniss = new Bruch(nzaehler, nnenner);
-		//System.out.println(bruchb2.zaehler+"/"+bruchb2.nenner);
+		berechnungen++;
 		return ergebniss;
 	}
 	public Bruch multiplicate2(Bruch b2,Bruch b3){
-		int nzaehler;
-		int nnenner;
-		nzaehler= this.zaehler * b2.zaehler * b3.zaehler;
-		nnenner= this.nenner * b2.nenner * b3.nenner;
-		Bruch ergebniss = new Bruch(nzaehler, nnenner);
-		//System.out.println(bruchb3.zaehler+"/"+bruchb3.nenner);
-		return ergebniss;
-	}
+		berechnungen++;
+		return new Bruch(this.zaehler*b2.zaehler*b3.zaehler, this.nenner*b2.nenner*b3.nenner);
+	} //Stark optimierte Rückgabe
 
+	public Bruch kuerzen(){
+/*	for (int repeat =0;repeat <11;repeat++){
+		for (int i = 2; i<11; i++){
+		if (nenner%i == 0 && zaehler%i ==0){
+			nenner=nenner / i;
+			zaehler=zaehler /i;
+			berechnungen++;
+		}
+		else continue;		
+	
+	}*/
+	int i=2;
+	while (i<nenner/2)
+	{
+		if (nenner%i == 0 && zaehler%i ==0){
+			nenner=nenner / i;
+			zaehler=zaehler /i;
+			berechnungen++;
+			i++;
+	}
+		else i++;
+	}
+	return new Bruch (zaehler,nenner);
+}
 }
--- src/Uebung1.java
+++ src/Uebung1.java
@@ -4,9 +4,12 @@ public class Uebung1 {
 	public static void main(String[] args) 
 	{
 		double ergebniss;
-		Bruch Test = new Bruch(7,3);
-		Bruch Test2 = new Bruch(3,5);
-		Bruch Test3 = new Bruch(1,2);
+		Bruch Test = new Bruch(420,45);
+		Bruch Test2 = new Bruch(240,720);
+		Bruch Test3 = new Bruch(100,100);
+		Bruch b1 = new Bruch(127,1);
+		Bruch b2 = new Bruch(1,127);
+		Bruch erg2 = new Bruch(1,1);
 		
 		Test.print();
 		
@@ -17,6 +20,13 @@ public class Uebung1 {
 		
 		erg= Test3.multiplicate2(Test, Test2);
 		erg.print();
+		
+		erg2=erg.kuerzen();
+		erg2.print();
+		System.out.println(erg2.toDecimal());
+		
+		System.out.println("Es befinden sich "+Bruch.Counter+ " Brüche im System.");
+		System.out.println("Es wurden "+Bruch.berechnungen +" Berechnungen durchgeführt");
 		
 	}
 
